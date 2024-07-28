be4e2a379462cd94cf3aa15049132e2615ac90cc
aaglis <aglissilva17@gmail.com>
2024-07-28 11:08:55 -0300

    fix(auth): '/login' and '/' endpoint should be public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './auth/decorators/is-public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
