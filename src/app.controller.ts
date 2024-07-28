eca537061437e670118793d750cb18b21336cdc8
aaglis <aglissilva17@gmail.com>
2024-07-28 11:56:48 -0300

    feat(auth): implement login validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { IsPublic } from './auth/decorators/is-public.decorator';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
+import { User } from './user/entities/user.entity';
 
 @Controller()
 export class AppController {
@@ -10,5 +12,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: User): User {
+    return user;
   }
 }
